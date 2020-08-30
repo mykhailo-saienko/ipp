1b94df6be2ed87d2fdcee745d03b3316cc1a285e
mischa <mischa@System-3>
2020-08-30 23:51:22 +0200

    minor warning corrections and re-formatting

--- test/src/ms/ipp/iterable/TestIterable.java
+++ test/src/ms/ipp/iterable/TestIterable.java
@@ -25,121 +25,125 @@ import ms.ipp.iterator.MappedIterator;
 import ms.ipp.iterator.NestedIterator;
 
 public class TestIterable {
-	private static void testNestedFiltered() {
-		// TODO: Can we extract any tests from it?
-		List<Map<String, Integer>> i = new ArrayList<>();
-		i.add(Iterables.makeMap(new String[] { "a", "b" }, new Integer[] { 1, 2 }));
-		i.add(Iterables.makeMap(new String[] { "a", "c" }, new Integer[] { 3, 4 }));
+    @SuppressWarnings("unused")
+    private static void testNestedFiltered() {
+        // TODO: Can we extract any tests from it?
+        List<Map<String, Integer>> i = new ArrayList<>();
+        i.add(Iterables.makeMap(new String[] { "a", "b" }, new Integer[] { 1, 2 }));
+        i.add(Iterables.makeMap(new String[] { "a", "c" }, new Integer[] { 3, 4 }));
 
-		Iterator<Map.Entry<String, Integer>> it = new NestedIterator<>(i.iterator(), m -> m.entrySet().iterator());
-		it = FilteredIterator.distinct(it, Entry::getKey);
-		it.next();
-		System.out.println(i);
-		it.remove();
-		System.out.println(i);
-	}
+        Iterator<Map.Entry<String, Integer>> it
+                = new NestedIterator<>(i.iterator(), m -> m.entrySet().iterator());
+        it = FilteredIterator.distinct(it, Entry::getKey);
+        it.next();
+        System.out.println(i);
+        it.remove();
+        System.out.println(i);
+    }
 
-	@Test
-	public void testNested() {
-		Map<String, List<Integer>> source = new TreeMap<>();
-		source.put("a", new ArrayList<>());
-		source.put("b", new ArrayList<>(Arrays.asList(1)));
-		source.put("c", new ArrayList<>());
-		source.put("d", new ArrayList<>(Arrays.asList(1, 2)));
-		source.put("e", new ArrayList<>());
-		BiIterable<String, Integer> it = createNested(source);
+    @Test
+    public void testNested() {
+        Map<String, List<Integer>> source = new TreeMap<>();
+        source.put("a", new ArrayList<>());
+        source.put("b", new ArrayList<>(Arrays.asList(1)));
+        source.put("c", new ArrayList<>());
+        source.put("d", new ArrayList<>(Arrays.asList(1, 2)));
+        source.put("e", new ArrayList<>());
+        BiIterable<String, Integer> it = createNested(source);
 
-		// Counting
-		Assert.assertEquals(3, count(it));
+        // Counting
+        Assert.assertEquals(3, count(it));
 
-		// Retrieval
-		assertIterator(it.iterator(), KVP("b", 1), KVP("d", 1), KVP("d", 2));
+        // Retrieval
+        assertIterator(it.iterator(), KVP("b", 1), KVP("d", 1), KVP("d", 2));
 
-		// Remove fist
-		Iterator<Entry<String, Integer>> iterator = it.iterator();
-		iterator.next();
-		iterator.remove();
-		assertIterator(iterator, KVP("d", 1), KVP("d", 2));
-		assertIterator(it.iterator(), KVP("d", 1), KVP("d", 2));
+        // Remove fist
+        Iterator<Entry<String, Integer>> iterator = it.iterator();
+        iterator.next();
+        iterator.remove();
+        assertIterator(iterator, KVP("d", 1), KVP("d", 2));
+        assertIterator(it.iterator(), KVP("d", 1), KVP("d", 2));
 
-		// Remove last
-		iterator = it.iterator();
-		iterator.next();
-		iterator.next();
-		iterator.remove();
-		assertIterator(iterator);
-		assertIterator(it.iterator(), KVP("d", 1));
+        // Remove last
+        iterator = it.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.remove();
+        assertIterator(iterator);
+        assertIterator(it.iterator(), KVP("d", 1));
 
-		// Remove before calling next should result into an IllegalStateError
-		assertThrows(IllegalStateException.class, () -> it.iterator().remove());
-	}
+        // Remove before calling next should result into an IllegalStateError
+        assertThrows(IllegalStateException.class, () -> it.iterator().remove());
+    }
 
-	private BiIterable<String, Integer> createNested(Map<String, List<Integer>> source) {
-		return () -> new NestedIterator<>(source.entrySet().iterator(),
-				e -> new MappedIterator<>(e.getValue().iterator(), i -> new KeyValue<>(e.getKey(), i)));
-	}
+    private BiIterable<String, Integer> createNested(Map<String, List<Integer>> source) {
+        return () -> new NestedIterator<>(source.entrySet().iterator(),
+                                          e -> new MappedIterator<>(e.getValue()
+                                                  .iterator(), i -> new KeyValue<>(e.getKey(), i)));
+    }
 
-	@Test
-	public void testEmptyNested() {
-		Map<String, List<Integer>> source = new TreeMap<>();
-		source.put("a", new ArrayList<>());
-		BiIterable<String, Integer> it = createNested(source);
-		// Counting
-		Assert.assertEquals(0, count(it));
-		Assert.assertTrue(!it.iterator().hasNext());
-		// Remove before calling next should result into an IllegalStateError
-		assertThrows(IllegalStateException.class, () -> it.iterator().remove());
-	}
+    @Test
+    public void testEmptyNested() {
+        Map<String, List<Integer>> source = new TreeMap<>();
+        source.put("a", new ArrayList<>());
+        BiIterable<String, Integer> it = createNested(source);
+        // Counting
+        Assert.assertEquals(0, count(it));
+        Assert.assertTrue(!it.iterator().hasNext());
+        // Remove before calling next should result into an IllegalStateError
+        assertThrows(IllegalStateException.class, () -> it.iterator().remove());
+    }
 
-	@Test
-	public void testDistinct() {
-		BiIterable<String, Integer> distinct = createTestDistinct();
+    @Test
+    public void testDistinct() {
+        BiIterable<String, Integer> distinct = createTestDistinct();
 
-		// Counting
-		Assert.assertEquals(3, count(distinct));
+        // Counting
+        Assert.assertEquals(3, count(distinct));
 
-		// Retrieval
-		assertIterator(distinct.iterator(), KVP("a", 1), KVP("b", 2), KVP("c", 4));
+        // Retrieval
+        assertIterator(distinct.iterator(), KVP("a", 1), KVP("b", 2), KVP("c", 4));
 
-		// Remove first
-		Iterator<Entry<String, Integer>> it = distinct.iterator();
-		it.next();
-		it.remove();
-		// although this iterator has deleted the KVP a=1...
-		assertIterator(it, KVP("b", 2), KVP("c", 4));
-		// ...globally, we have another KVP with the same key a=3.
-		assertIterator(distinct.iterator(), KVP("b", 2), KVP("a", 3), KVP("c", 4));
+        // Remove first
+        Iterator<Entry<String, Integer>> it = distinct.iterator();
+        it.next();
+        it.remove();
+        // although this iterator has deleted the KVP a=1...
+        assertIterator(it, KVP("b", 2), KVP("c", 4));
+        // ...globally, we have another KVP with the same key a=3.
+        assertIterator(distinct.iterator(), KVP("b", 2), KVP("a", 3), KVP("c", 4));
 
-		// Remove last
-		it = distinct.iterator();
-		it.next();
-		it.next();
-		it.next();
-		it.remove();
-		// no values must remain in the iterator
-		assertIterator(it);
-		// ... but two values are still in the iterable
-		assertIterator(distinct.iterator(), KVP("b", 2), KVP("a", 3));
+        // Remove last
+        it = distinct.iterator();
+        it.next();
+        it.next();
+        it.next();
+        it.remove();
+        // no values must remain in the iterator
+        assertIterator(it);
+        // ... but two values are still in the iterable
+        assertIterator(distinct.iterator(), KVP("b", 2), KVP("a", 3));
 
-		// Remove before calling next should result into an IllegalStateError
-		assertThrows(IllegalStateException.class, () -> distinct.iterator().remove());
-	}
+        // Remove before calling next should result into an IllegalStateError
+        assertThrows(IllegalStateException.class, () -> distinct.iterator().remove());
+    }
 
-	private BiIterable<String, Integer> createTestDistinct() {
-		List<Entry<String, Integer>> map = new ArrayList<>(asList(KVP("a", 1), KVP("b", 2), KVP("a", 3), KVP("c", 4)));
-		return toBiIt(distinct(map, Entry::getKey));
-	}
+    private BiIterable<String, Integer> createTestDistinct() {
+        List<Entry<String, Integer>> map
+                = new ArrayList<>(asList(KVP("a", 1), KVP("b", 2), KVP("a", 3), KVP("c", 4)));
+        return toBiIt(distinct(map, Entry::getKey));
+    }
 
-	@SafeVarargs
-	public static <T> void assertIterator(Iterator<T> it, T... entries) {
-		for (T t : entries) {
-			Assert.assertEquals(it.next(), t);
-		}
-		Assert.assertTrue(!it.hasNext());
-	}
+    @SafeVarargs
+    public static <T> void assertIterator(Iterator<T> it, T... entries) {
+        for (T t : entries) {
+            Assert.assertEquals(it.next(), t);
+        }
+        Assert.assertTrue(!it.hasNext());
+    }
 
-	public static <T, U> void assertKV(T expKey, U expValue, Entry<T, U> entry) {
-		Assert.assertEquals(expKey, entry.getKey());
-		Assert.assertEquals(expValue, entry.getValue());
-	}
+    public static <T, U> void assertKV(T expKey, U expValue, Entry<T, U> entry) {
+        Assert.assertEquals(expKey, entry.getKey());
+        Assert.assertEquals(expValue, entry.getValue());
+    }
 }
