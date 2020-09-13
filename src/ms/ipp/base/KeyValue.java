e5a0dd9725e2f4ec6e1ae741a1f98637c76f8c7f
mischa <mischa@192.168.0.81>
2020-09-13 14:50:54 +0200

    [kvp] Reformatted according to new formatting rules

--- src/ms/ipp/base/KeyValue.java
+++ src/ms/ipp/base/KeyValue.java
@@ -14,80 +14,80 @@ import java.util.Map;
  * @param <V>
  */
 public class KeyValue<T, V> implements Map.Entry<T, V> {
-	private T key;
-	private V value;
+    private T key;
+    private V value;
 
-	public static <U, W> KeyValue<U, W> KVP(U key, W value) {
-		return new KeyValue<U, W>(key, value);
-	}
+    public static <U, W> KeyValue<U, W> KVP(U key, W value) {
+        return new KeyValue<U, W>(key, value);
+    }
 
-	/**
-	 * Creates a new KeyValue with both the key and the value equal to null.
-	 */
-	public KeyValue() {
-		this(null, null);
-	}
+    /**
+     * Creates a new KeyValue with both the key and the value equal to null.
+     */
+    public KeyValue() {
+        this(null, null);
+    }
 
-	/**
-	 * Creates a copy of existing {@code Map.Entry}.
-	 * 
-	 * @param source the source Map.Entry, not null
-	 */
-	public KeyValue(Map.Entry<T, V> source) {
-		this(source.getKey(), source.getValue());
-	}
+    /**
+     * Creates a copy of existing {@code Map.Entry}.
+     * 
+     * @param source the source Map.Entry, not null
+     */
+    public KeyValue(Map.Entry<T, V> source) {
+        this(source.getKey(), source.getValue());
+    }
 
-	/**
-	 * Creates a new KeyValue with given key and value.
-	 * 
-	 * @param key
-	 * @param value
-	 */
-	public KeyValue(T key, V value) {
-		this.key = key;
-		this.value = value;
-	}
+    /**
+     * Creates a new KeyValue with given key and value.
+     * 
+     * @param key
+     * @param value
+     */
+    public KeyValue(T key, V value) {
+        this.key = key;
+        this.value = value;
+    }
 
-	@Override
-	public V setValue(V value) {
-		return this.value = value;
-	}
+    @Override
+    public V setValue(V value) {
+        return this.value = value;
+    }
 
-	@Override
-	public V getValue() {
-		return value;
-	}
+    @Override
+    public V getValue() {
+        return value;
+    }
 
-	/**
-	 * Sets a new key.
-	 * 
-	 * @param key
-	 */
-	public void setKey(T key) {
-		this.key = key;
-	}
+    /**
+     * Sets a new key.
+     * 
+     * @param key
+     */
+    public void setKey(T key) {
+        this.key = key;
+    }
 
-	@Override
-	public T getKey() {
-		return key;
-	}
+    @Override
+    public T getKey() {
+        return key;
+    }
 
-	@Override
-	public int hashCode() {
-		return (key == null ? 0 : key.hashCode()) ^ (value == null ? 0 : value.hashCode());
-	}
+    @Override
+    public int hashCode() {
+        return (key == null ? 0 : key.hashCode()) ^ (value == null ? 0 : value.hashCode());
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof KeyValue)) {
-			return false;
-		}
-		KeyValue<?, ?> k = (KeyValue<?, ?>) obj;
-		return isEqualOrNull(key, k.key) && isEqualOrNull(value, k.value);
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof KeyValue)) {
+            return false;
+        }
+        KeyValue<?, ?> k = (KeyValue<?, ?>) obj;
+        return isEqualOrNull(key, k.key) && isEqualOrNull(value, k.value);
+    }
 
-	@Override
-	public String toString() {
-		return key + ": " + value;
-	}
+    @Override
+    public String toString() {
+        return key + ": " + value;
+    }
 }
